d68bce02a6008e0490d3f5076bfe2743798755fb
KenjiDoom <vitodevcool@protonmail.com>
2021-02-15 07:02:46 +0000

    nikto and nmap done

--- made_ez.py
+++ made_ez.py
@@ -1,13 +1,15 @@
 # This script will automate begging tasks for a ctf
 import subprocess
 import os
+# 45.33.32.156
+# Test IP, Permission given by nmap
+
+
 
 def main():
     IP = input("Enter IP: ")
-    print ("Scanning on " + IP) 
-    #subprocess.run(["nmap", "-A" + IP])
-    os.system(f"nmap -A {IP}")
-
+    subprocess.run(["nmap", "-A", "-sC", "-sV", "-oN", "nmap_scan_results.txt", IP])
+    subprocess.run(["nikto", "-o", "nikto_scan_resutls.txt", "-h", IP])
 
 
 
