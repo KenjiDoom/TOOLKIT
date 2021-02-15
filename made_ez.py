7ff4bd4fc858f49891f79e63aedbb59afde38add
KenjiDoom <vitodevcool@protonmail.com>
2021-02-15 05:49:06 +0000

    nmap add

--- made_ez.py
+++ made_ez.py
@@ -1,9 +1,16 @@
 # This script will automate begging tasks for a ctf
-
+import subprocess
+import os
 
 def main():
     IP = input("Enter IP: ")
-    print(IP)
+    print ("Scanning on " + IP) 
+    #subprocess.run(["nmap", "-A" + IP])
+    os.system(f"nmap -A {IP}")
+
+
+
+
 
 
 if __name__ == "__main__":
