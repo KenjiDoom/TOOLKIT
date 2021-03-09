08d9aefeb4f44a07640af4a9cec8c94627d1dada
KenjiDoom <vitodevcool@protonmail.com>
2021-03-09 09:51:41 +0000

    simple changes

--- made_ez.py
+++ made_ez.py
@@ -3,14 +3,11 @@ import subprocess
 import os
 # 45.33.32.156
 # Test IP, Permission given by nmap
-
-def brute():
-    pass
-
-
+# These are the list of tools that are commonly used when enumerating 
 
 def enum():
     IP = input("Enter IP: ")
+    subprocess.run(["wpscan", "--url", IP])
     subprocess.run(["nmap", "-A", "-sC", "-sV", "-oN", "nmap_scan_results.txt", IP])
     subprocess.run(["nikto", "-o", "nikto_scan_resutls.txt", "-h", IP])
     subprocess.run(["enum4linux", "-a", IP])
@@ -18,4 +15,5 @@ def enum():
 
 
 if __name__ == "__main__":
-    enum()
+    while True:
+        enum()
