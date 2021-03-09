a6ab76659bc23229c8da9d45d737ee3cf91cb6f8
KenjiDoom <vitodevcool@protonmail.com>
2021-03-09 09:41:13 +0000

    new program added

--- made_ez.py
+++ made_ez.py
@@ -4,16 +4,18 @@ import os
 # 45.33.32.156
 # Test IP, Permission given by nmap
 
+def brute():
+    pass
 
 
-def main():
+
+def enum():
     IP = input("Enter IP: ")
     subprocess.run(["nmap", "-A", "-sC", "-sV", "-oN", "nmap_scan_results.txt", IP])
     subprocess.run(["nikto", "-o", "nikto_scan_resutls.txt", "-h", IP])
-
-
+    subprocess.run(["enum4linux", "-a", IP])
 
 
 
 if __name__ == "__main__":
-    main()
+    enum()
